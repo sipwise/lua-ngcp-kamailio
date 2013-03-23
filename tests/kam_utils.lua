50c4ea25670a31a00dbef8602ac6acf9ff66c490
Victor Seva <linuxmaniac@torreviejawireless.org>
2013-03-23 22:22:40 +0100

    Fix kam_utils tests

--- tests/kam_utils.lua
+++ tests/kam_utils.lua
@@ -8,34 +8,39 @@ sr = srMock:new()
 TestKamUtils = {} --class
 
     function TestKamUtils:setUp()
-        self.list_ids = {
+        self.list_idsi = {
             id1 = 1,
             id2 = 2,
             id3 = 3
         }
+        self.list_idss = {
+            id1 = "1",
+            id2 = "2",
+            id3 = "3"
+        }
     end
 
     function TestKamUtils:test_table_log()
-        table.log(self.list_ids, "list ids")
-        table.log(self.list_ids, "list ids", "info")
+        table.log(self.list_idsi, "list ids")
+        table.log(self.list_idsi, "list ids", "info")
         table.log(nil, "list ids")
-        table.log(self.list_ids, "list ids", nil)
-        table.log(self.list_ids, nil)
+        table.log(self.list_idss, "list ids", nil)
+        table.log(self.list_idss, nil)
     end
 
     function TestKamUtils:test_sets_avps()
         assertError(sets_avps, nil)
-        sets_avps(self.list_ids)
+        sets_avps(self.list_idss)
     end
 
     function TestKamUtils:test_seti_avps()
         assertError(seti_avps, nil)
-        seti_avps(self.list_ids)
+        seti_avps(self.list_idsi)
     end
 
     function TestKamUtils:test_clean_avp()
         assertError(clean_avp, nil)
-        clean_avp(self.list_ids)
+        clean_avp(self.list_idsi)
         clean_avp("testid")
     end
 -- class TestKamUtils
