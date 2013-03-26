af52bc14713e383f86c721f5918c04ab494c64ac
Victor Seva <linuxmaniac@torreviejawireless.org>
2013-03-26 18:11:02 +0100

    Fix lemock use on NGCPUserPrefs test

--- tests/ngcp_up.lua
+++ tests/ngcp_up.lua
@@ -2,31 +2,31 @@
 require('luaunit')
 require 'mocks.sr'
 require 'ngcp.utils'
+require 'tests_v.up_vars'
 
 sr = srMock:new()
-
-local mc = lemock.controller()
-local config = mc:mock()
-local mysql = mc:mock()
-local env = mc:mock()
-local con = mc:mock()
-local cur = mc:mock()
-
-package.loaded.luasql = nil
-package.preload['luasql.mysql'] = function ()
-    luasql = {}
-    luasql.mysql = mysql
-    return mysql
-end
-
-require 'ngcp.up'
-require 'tests_v.up_vars'
+local mc = nil
 
 TestNGCPUserPrefs = {} --class
 
     function TestNGCPUserPrefs:setUp()
-        --print("TestNGCPUserPrefs:setUp")
-        self.d = NGCPUserPrefs:new(config)
+        mc = lemock.controller()
+        self.config = mc:mock()
+        self.mysql = mc:mock()
+        self.env = mc:mock()
+        self.con = mc:mock()
+        self.cur = mc:mock()
+
+        package.loaded.luasql = nil
+        package.preload['luasql.mysql'] = function ()
+            luasql = {}
+            luasql.mysql = mysql
+            return mysql
+        end
+
+        require 'ngcp.up'
+
+        self.d = NGCPUserPrefs:new(self.config)
     end
 
     function TestNGCPUserPrefs:tearDown()
@@ -40,14 +40,33 @@ TestNGCPUserPrefs = {} --class
 
     function TestNGCPUserPrefs:test_caller_load()
         assertTrue(self.d.config)
-        config:getDBConnection() ;mc :returns(con)
-        con:execute("SELECT * FROM usr_preferences WHERE uuid ='ae736f72-21d1-4ea6-a3ea-4d7f56b3887c'")  ;mc :returns(cur)
-        cur:fetch(mc.ANYARGS)    ;mc :returns(up_vars["ae736f72_21d1_4ea6_a3ea_4d7f56b3887c"])
-        cur:close()
-        con:close()
+        self.config:getDBConnection() ;mc :returns(self.con)
+        self.con:execute("SELECT * FROM usr_preferences WHERE uuid ='ae736f72-21d1-4ea6-a3ea-4d7f56b3887c'")  ;mc :returns(self.cur)
+        self.cur:fetch(mc.ANYARGS)    ;mc :returns(up_vars["ae736f72_21d1_4ea6_a3ea_4d7f56b3887c"])
+        self.cur:close()
+        self.con:close()
 
         mc:replay()
         self.d:caller_load("ae736f72-21d1-4ea6-a3ea-4d7f56b3887c")
+        mc:verify()
+
+        assertTrue(self.d.xavp)
+        assertEquals(self.d.xavp("cli"),"4311001")
+        assertEquals(self.d.xavp("cc"),"43")
+        assertEquals(self.d.xavp("ac"),"1")
+        assertEquals(self.d.xavp("cli"),"4311001")
+    end
+
+    function TestNGCPUserPrefs:test_callee_load()
+        assertTrue(self.d.config)
+        self.config:getDBConnection() ;mc :returns(self.con)
+        self.con:execute("SELECT * FROM usr_preferences WHERE uuid ='ae736f72-21d1-4ea6-a3ea-4d7f56b3887c'")  ;mc :returns(self.cur)
+        self.cur:fetch(mc.ANYARGS)    ;mc :returns(up_vars["ae736f72_21d1_4ea6_a3ea_4d7f56b3887c"])
+        self.cur:close()
+        self.con:close()
+
+        mc:replay()
+        self.d:callee_load("ae736f72-21d1-4ea6-a3ea-4d7f56b3887c")
         mc:verify()
 
         assertTrue(self.d.xavp)
