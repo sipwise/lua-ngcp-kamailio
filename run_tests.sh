f33239a683e89fda051a5413d6855b917cc2dd73
Victor Seva <linuxmaniac@torreviejawireless.org>
2013-04-05 09:16:12 +0200

    Just run one single call to all tests.

--- run_tests.sh
+++ run_tests.sh
@@ -3,8 +3,10 @@
 mkdir -p reports
 rm -rf reports/*
 
-for f in tests/test_*.lua; do
-    NAME=$(basename ${f} .lua)
-    lua5.1 ${f} > reports/${NAME}.tap
-done
+#for f in tests/test_*.lua; do
+#    NAME=$(basename ${f} .lua)
+#    lua5.1 ${f} > reports/${NAME}.tap
+#done
+
+lua5.1 tests/test_all.lua > reports/test_all.tap
 #EOF
