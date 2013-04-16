bc2286d5d263b66410880decbcf58f6a30065ba4
Victor Seva <linuxmaniac@torreviejawireless.org>
2013-04-16 14:15:22 +0200

    prefs.lua is now in templates.

--- copy_templates.sh
+++ copy_templates.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cp prefs.lua.tt2 /etc/ngcp-config/templates/etc/kamailio/proxy/
+tpage --define PRO=true templates/proxy/prefs.lua > /etc/ngcp-config/templates/etc/kamailio/proxy/prefs.lua.tt2
 tpage --define PRO=true templates/proxy/kamailio.cfg > /etc/ngcp-config/templates/etc/kamailio/proxy/kamailio.cfg.customtt.tt2
 tpage --define PRO=true templates/proxy/proxy.cfg > /etc/ngcp-config/templates/etc/kamailio/proxy/proxy.cfg.customtt.tt2
 ngcpcfg build /etc/kamailio/proxy
