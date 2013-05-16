be5cbde78206cdaa6f0d24ec118a1341672afe8d
Victor Seva <linuxmaniac@torreviejawireless.org>
2013-05-16 17:56:31 +0200

    Add test case for test the value of force_outbound_calls_to_peer pref and caller_force_outbound_calls_to_peer (cherry picked from commit 0ba8d41f4566dd01f2849915662ea29bfa1aa941)

--- tests_v/up_vars.lua
+++ tests_v/up_vars.lua
@@ -49,6 +49,16 @@ up_vars = {
           type = 0,
           value = "no",
           last_modified = "1900-01-01 00:00:01"
+        },
+        {
+          id = 11,
+          uuid = "ae736f72-21d1-4ea6-a3ea-4d7f56b3887c",
+          username = "testuser1",
+          domain = "192.168.51.56",
+          attribute = "force_outbound_calls_to_peer",
+          type = 1,
+          value = 1,
+          last_modified = "1900-01-01 00:00:01"
         }
     },
     _94023caf_dfba_4f33_8bdb_b613ce627613 = {
