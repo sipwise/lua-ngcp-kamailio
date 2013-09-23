aaa7bbb1c89f75696834dcb35bcfa64a3db8c30a
Victor Seva <vseva@sipwise.com>
2013-09-23 14:54:07 +0200

    run_test.sh can be called with a list of tests to run.

--- run_tests.sh
+++ run_tests.sh
@@ -3,10 +3,25 @@
 mkdir -p reports
 rm -rf reports/*
 
-#for f in tests/test_*.lua; do
-#    NAME=$(basename ${f} .lua)
-#    lua5.1 ${f} > reports/${NAME}.tap
-#done
+if [[ ! -z "$@" ]]; then
+	for i in $@; do
+		f="tests/$i.lua"
+		if [ ! -f $f ]; then
+			echo "No $f found"
+		else
+			echo "testing $f -> reports/${i}.tap"
+			cat<<EOF|lua5.1 - > reports/${i}.tap
+require "tests/$i"
+---- Control test output:
+lu = LuaUnit
+lu:setOutputType('TAP')
+lu:setVerbosity(1)
+lu:run()
+EOF
+		fi
+	done
+	exit 0
+fi
 
 lua5.1 tests/test_all.lua > reports/test_all.tap
 #EOF
