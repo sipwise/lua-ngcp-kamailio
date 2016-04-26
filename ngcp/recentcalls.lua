6b840a25661def4b1d051adabec985b747fbd7a5
Victor Seva <vseva@sipwise.com>
2016-04-26 09:44:50 +0200

    MT#19341 [FAKE] upgrade copyright
    Change-Id: Iafdef4fd2af73df04fef46bb799c5f4b7159564f

--- ngcp/recentcalls.lua
+++ ngcp/recentcalls.lua
@@ -1,5 +1,5 @@
 --
--- Copyright 2015 SipWise Team <development@sipwise.com>
+-- Copyright 2015-2016 SipWise Team <development@sipwise.com>
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
--- ngcp/rp.lua
+++ ngcp/rp.lua
@@ -1,5 +1,5 @@
 --
--- Copyright 2013 SipWise Team <development@sipwise.com>
+-- Copyright 2013-2016 SipWise Team <development@sipwise.com>
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
