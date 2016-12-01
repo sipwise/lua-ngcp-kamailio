0c6e3ae91f25c2cb7b3c53f5cdf4e63a8fc17c25
Victor Seva <vseva@sipwise.com>
2016-12-01 17:05:46 +0100

    TT#7190 ngcp-dlgcnt-check
    * fix default redis db when no config file
    * fix call-id list from kamailio
    * refesh Docker due to TT#5032
    
    Change-Id: Ie8847b238e5a24b22baa48fc8c9b41e39397d84e
    (cherry picked from commit d80e64fd298499ceb262c96e3ab3e4e6c69012aa)

--- tests/fixtures/koredis.cmd
+++ tests/fixtures/koredis.cmd
@@ -1,3 +1,3 @@
-ngcp-dlgcnt-clean f81f8a33-2dbd-1235-018c-0090fb484030-pbx_1
-ngcp-dlgcnt-clean f81f8a33-2dbd-1235-018c-0090fb484030
-ngcp-dlglist-clean f81f8a33-2dbd-1235-018c-0090fb484030
+ngcp-dlgcnt-clean f81f8a33-2dbd-1235-018c-0090fb484030:5060-pbx_1
+ngcp-dlgcnt-clean f81f8a33-2dbd-1235-018c-0090fb484030:5060
+ngcp-dlglist-clean f81f8a33-2dbd-1235-018c-0090fb484030:5060
