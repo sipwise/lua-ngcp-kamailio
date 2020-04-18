48b0d4d018de42700163dafeac3be0891a5afa6a
Victor Seva <vseva@sipwise.com>
2020-04-18 18:05:43 +0200

    TT#78651 Refactor code
    * use proper class inherance
    
    Change-Id: I69c9b14c64762ec1a1e17267961830f4794f2246

--- ngcp/ngcp.lua
+++ ngcp/ngcp.lua
@@ -1,5 +1,5 @@
 --
--- Copyright 2013 SipWise Team <development@sipwise.com>
+-- Copyright 2013-2020 SipWise Team <development@sipwise.com>
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
@@ -31,144 +31,138 @@ local utils = require 'ngcp.utils'
 local utable = utils.table
 
 -- class NGCP
-local NGCP = {
-     __class__ = 'NGCP'
-}
-local NGCP_MT = { __index = NGCP }
-
-NGCP_MT.__tostring = function (t)
+local NGCP = utils.inheritsFrom()
+-- luacheck: globals KSR
+function NGCP:__tostring()
     local output = ''
-    for _,v in pairs(t.prefs) do
+    for _,v in pairs(self.prefs) do
         output = output .. tostring(v)
     end
     return output
 end
 
-    function NGCP:new()
-        local t = NGCP.init()
-        setmetatable( t, NGCP_MT )
-        return t
+function NGCP:new()
+    local instance = NGCP:create()
+    instance:init()
+    return instance
+end
+
+function NGCP:init()
+    self.config = NGCPConfig:new()
+    self.prefs = {
+        dom      = NGCPDomainPrefs:new(self.config),
+        prof     = NGCPProfilePrefs:new(self.config),
+        usr      = NGCPUserPrefs:new(self.config),
+        peer     = NGCPPeerPrefs:new(self.config),
+        real     = NGCPRealPrefs:new(self.config),
+        contract = NGCPContractPrefs:new(self.config),
+        fax      = NGCPFaxPrefs:new(self.config),
+    }
+end
+
+function NGCP:caller_contract_load(contract, ip)
+    local keys = self.prefs.contract:caller_load(contract, ip)
+
+    self.prefs.real:caller_contract_load(keys)
+    return keys
+end
+
+function NGCP:callee_contract_load(contract, ip)
+    local keys = self.prefs.contract:callee_load(contract, ip)
+
+    self.prefs.real:callee_contract_load(keys)
+    return keys
+end
+
+function NGCP:caller_peer_load(peer)
+    local keys = self.prefs.peer:caller_load(peer)
+
+    self.prefs.real:caller_peer_load(keys)
+    return keys
+end
+
+function NGCP:callee_peer_load(peer)
+    local keys = self.prefs.peer:callee_load(peer)
+
+    self.prefs.real:callee_peer_load(keys)
+    return keys
+end
+
+function NGCP:caller_usr_load(uuid, domain)
+    local keys = {
+        domain = self.prefs.dom:caller_load(domain),
+        prof   = self.prefs.prof:caller_load(uuid),
+        user   = self.prefs.usr:caller_load(uuid),
+        fax    = self.prefs.fax:caller_load(uuid)
+    }
+    local unique_keys = utable.deepcopy(keys.domain)
+    utable.merge(unique_keys, keys.prof)
+    utable.merge(unique_keys, keys.user)
+
+    self.prefs.real:caller_usr_load(unique_keys)
+    NGCPXAvp:new('caller', 'dom')
+
+    return unique_keys
+end
+
+function NGCP:callee_usr_load(uuid, domain)
+    local keys = {
+        domain = self.prefs.dom:callee_load(domain),
+        prof   = self.prefs.prof:callee_load(uuid),
+        user   = self.prefs.usr:callee_load(uuid),
+        fax    = self.prefs.fax:callee_load(uuid)
+    }
+    local unique_keys = utable.deepcopy(keys.domain)
+    utable.merge(unique_keys, keys.prof)
+    utable.merge(unique_keys, keys.user)
+
+    self.prefs.real:callee_usr_load(unique_keys)
+
+    return unique_keys
+end
+
+function NGCP:log_pref(level, vtype)
+    local xavp,xavp_log
+    local msg = "%s:%s\n"
+    if not level then
+        level = "dbg"
     end
 
-    function NGCP.init()
-        local t = {
-            config = NGCPConfig:new()
-        }
-        t.prefs = {
-            dom      = NGCPDomainPrefs:new(t.config),
-            prof     = NGCPProfilePrefs:new(t.config),
-            usr      = NGCPUserPrefs:new(t.config),
-            peer     = NGCPPeerPrefs:new(t.config),
-            real     = NGCPRealPrefs:new(t.config),
-            contract = NGCPContractPrefs:new(t.config),
-            fax      = NGCPFaxPrefs:new(t.config),
-        }
-        return t
-    end
-
-    function NGCP:caller_contract_load(contract, ip)
-        local keys = self.prefs.contract:caller_load(contract, ip)
-
-        self.prefs.real:caller_contract_load(keys)
-        return keys
-    end
-
-    function NGCP:callee_contract_load(contract, ip)
-        local keys = self.prefs.contract:callee_load(contract, ip)
-
-        self.prefs.real:callee_contract_load(keys)
-        return keys
-    end
-
-    function NGCP:caller_peer_load(peer)
-        local keys = self.prefs.peer:caller_load(peer)
-
-        self.prefs.real:caller_peer_load(keys)
-        return keys
-    end
-
-    function NGCP:callee_peer_load(peer)
-        local keys = self.prefs.peer:callee_load(peer)
-
-        self.prefs.real:callee_peer_load(keys)
-        return keys
-    end
-
-    function NGCP:caller_usr_load(uuid, domain)
-        local keys = {
-            domain = self.prefs.dom:caller_load(domain),
-            prof   = self.prefs.prof:caller_load(uuid),
-            user   = self.prefs.usr:caller_load(uuid),
-            fax    = self.prefs.fax:caller_load(uuid)
-        }
-        local unique_keys = utable.deepcopy(keys.domain)
-        utable.merge(unique_keys, keys.prof)
-        utable.merge(unique_keys, keys.user)
-
-        self.prefs.real:caller_usr_load(unique_keys)
-        NGCPXAvp:new('caller', 'dom')
-
-        return unique_keys
-    end
-
-    function NGCP:callee_usr_load(uuid, domain)
-        local keys = {
-            domain = self.prefs.dom:callee_load(domain),
-            prof   = self.prefs.prof:callee_load(uuid),
-            user   = self.prefs.usr:callee_load(uuid),
-            fax    = self.prefs.fax:callee_load(uuid)
-        }
-        local unique_keys = utable.deepcopy(keys.domain)
-        utable.merge(unique_keys, keys.prof)
-        utable.merge(unique_keys, keys.user)
-
-        self.prefs.real:callee_usr_load(unique_keys)
-
-        return unique_keys
-    end
-
-    function NGCP:log_pref(level, vtype)
-        local xavp,xavp_log
-
-        if not level then
-            level = "dbg"
+    if not vtype then
+        for _,pref in pairs(self.prefs) do
+            xavp = pref:xavp("caller")
+            xavp_log = tostring(xavp)
+            KSR.log(level, msg:format(xavp.name, xavp_log))
+            xavp = pref:xavp("callee")
+            xavp_log = tostring(xavp)
+            KSR.log(level, msg:format(xavp.name, xavp_log))
         end
-
-        if not vtype then
-            for _,pref in pairs(self.prefs) do
-                xavp = pref:xavp("caller")
-                xavp_log = tostring(xavp)
-                KSR.log(level, string.format("%s:%s\n", xavp.name, xavp_log))
-                xavp = pref:xavp("callee")
-                xavp_log = tostring(xavp)
-                KSR.log(level, string.format("%s:%s\n", xavp.name, xavp_log))
-            end
+    else
+        if self.prefs[vtype] then
+            xavp = self.prefs[vtype]:xavp("caller")
+            xavp_log = tostring(xavp)
+            KSR.log(level, msg:format(xavp.name, xavp_log))
+            xavp = self.prefs[vtype]:xavp("callee")
+            xavp_log = tostring(xavp)
+            KSR.log(level, msg:format(xavp.name, xavp_log))
         else
-            if self.prefs[vtype] then
-                xavp = self.prefs[vtype]:xavp("caller")
-                xavp_log = tostring(xavp)
-                KSR.log(level, string.format("%s:%s\n", xavp.name, xavp_log))
-                xavp = self.prefs[vtype]:xavp("callee")
-                xavp_log = tostring(xavp)
-                KSR.log(level, string.format("%s:%s\n", xavp.name, xavp_log))
-            else
-                error(string.format("there is no prefs for %s", vtype))
-            end
+            error(string.format("there is no prefs for %s", vtype))
         end
     end
+end
 
-    function NGCP:clean(vtype, group)
-        if not group then
-            for _,v in pairs(self.prefs) do
-                v:clean(vtype)
-            end
+function NGCP:clean(vtype, group)
+    if not group then
+        for _,v in pairs(self.prefs) do
+            v:clean(vtype)
+        end
+    else
+        if self.prefs[group] then
+            self.prefs[group]:clean(vtype)
         else
-            if self.prefs[group] then
-                self.prefs[group]:clean(vtype)
-            else
-                error(string.format("unknown group:%s", group))
-            end
+            error(string.format("unknown group:%s", group))
         end
     end
+end
 -- class
 return NGCP
