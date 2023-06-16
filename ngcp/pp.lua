f7d7bcafe27fed45fc40c9b40cd71424a12443a0
Daniel Grotti <dgrotti@sipwise.com>
2023-06-16 11:32:51 +0200

    MT#56961 Clean $xavp(callee_prefs) when cleaning $xavp(callee_peer_prefs)
    The following scenario disclouse an issue with "ngcp_clean" lua
    function in case of callee peer:
    
    - A is a local subscriber with Peer_auth set and
      force_outbound_call_to_peer set;
    - B is a local subscriber with Peer_auth set and
      force_outbound_call_to_peer set;
    
    A calls B.
    We clean and load callee preferences of B.
    Call is detected as forced to PSTN.
    We clean and load peer preferences.
    When we set outbound peer_auth* values, the $xavp(callee_prefs)
    still contains values from B (instead of peering preferences).
    
    When we clean preferences for the callee peer, we only
    clean "callee_peer_prefs" and not "callee_prefs".
    "callee_prefs" are clean only in case of 'usr' or 'dom'
    preferences.
    
    This patch try to fix this problem in order to fix this
    scenario with outbound user authentication.
    
    Change-Id: I76dfa6f9e074b3d83ac67bd818e59233ca50492c
    (cherry picked from commit 8cdd63731bb0b5eeb8617e77d10d96170db58793)

--- ngcp/pp.lua
+++ ngcp/pp.lua
@@ -19,6 +19,7 @@
 --
 local utils = require 'ngcp.utils'
 local NGCPPrefs = require 'ngcp.pref'
+local NGCPXAvp = require 'ngcp.xavp'
 
 -- class NGCPPeerPrefs
 local NGCPPeerPrefs = utils.inheritsFrom(NGCPPrefs)
@@ -34,6 +35,16 @@ function NGCPPeerPrefs:new(config)
     -- creates xavp usr
     instance:init()
     return instance
+end
+
+function NGCPPeerPrefs:clean(vtype)
+    NGCPPrefs.clean(self, vtype)
+    if not vtype then
+        NGCPXAvp:new('callee', 'prefs'):clean()
+        NGCPXAvp:new('caller', 'prefs'):clean()
+    else
+        NGCPXAvp:new(vtype, 'prefs'):clean()
+    end
 end
 
 -- class
